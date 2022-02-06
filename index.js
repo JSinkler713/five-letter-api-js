7c8f597084da3bd3a2acc3e3ae7be83415365a24
James Sinkler <jamessinkler713@gmail.com>
2022-02-06 14:56:12 -0700

    adds cors

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const fiveLetterPackage = require('@jsinkler713/five-letter-word')
+const  cors = require('cors')
 
 const app = express()
+app.use(cors()) // allow all
 
 app.get('/five-letter', (req, res) => {
   res.json({word: fiveLetterPackage.getFiveLetterWord()})
