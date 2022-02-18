535c464b7be454eee6f4dc448b6e242232b25edc
James Sinkler <jamessinkler713@gmail.com>
2022-02-17 21:19:10 -0700

    install body-parser, check if a word

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 const express = require('express')
 const fiveLetterPackage = require('@jsinkler713/five-letter-word')
 const  cors = require('cors')
+var bodyParser = require('body-parser')
+
 
 const app = express()
 app.use(cors()) // allow all
+app.use(bodyParser.json())
 
 app.get('/five-letter', (req, res) => {
   res.json({word: fiveLetterPackage.getFiveLetterWord()})
+})
+
+app.post('/check-is-word', (req, res) => {
+  console.log(req.body)
+  res.json(fiveLetterPackage.checkForWord(req.body.word))
 })
 
 app.listen(4000, ()=> {
