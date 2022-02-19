67fe967409ab9edbd095e8f123d2a83a5033ec1a
James Sinkler <jamessinkler713@gmail.com>
2022-02-18 19:04:03 -0700

    Prettier and typo

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-const express = require('express')
-const fiveLetterPackage = require('@jsinkler713/five-letter-word')
-const cors = require('cors')
-var bodyParser = require('body-parser')
+const express = require("express");
+const fiveLetterPackage = require("@jsinkler713/five-letter-word");
+const cors = require("cors");
+var bodyParser = require("body-parser");
 
-const app = express()
-const PORT = process.env.PORT || 4000
-app.use(cors()) // allow all
-app.use(bodyParser.json())
+const app = express();
+const PORT = process.env.PORT || 4000;
+app.use(cors()); // allow all
+app.use(bodyParser.json());
 
-app.get('/five-letter', (req, res) => {
-  res.json({word: fiveLetterPackage.getFiveLetterWord()})
-})
+app.get("/five-letter", (req, res) => {
+  res.json({ word: fiveLetterPackage.getFiveLetterWord() });
+});
 
-app.post('/check-is-word', (req, res) => {
-  res.json(fiveLetterPackage.checkForWord(req.body.word))
-})
+app.post("/check-is-word", (req, res) => {
+  res.json(fiveLetterPackage.checkForWord(req.body.word));
+});
 
-app.listen(PORT, ()=> console.log(`listening on port ${PORT}`);
+app.listen(PORT, () => console.log(`listening on port ${PORT}`));
